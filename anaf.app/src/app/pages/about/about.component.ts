c08214dbf7f752258dd4fabb87f4b6b0e0f0fe32
George Claudiu Panainte <george.panainte@gmail.com>
2016-12-29 16:13:08 +0200

    Finish rss feed component; Cleanup about page from tests; Update configuration for Rss Feed Channel List; Doccumentation for Rss Feed Component

--- anaf.app/src/app/pages/about/about.component.ts
+++ anaf.app/src/app/pages/about/about.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { RssService } from '../../providers/rss.service';
-import { RssChannel, IRssChannel } from '../../models/rss-channel.model';
 
 @Component({
   templateUrl: 'about.component.html',
-  providers: [RssService]
 })
 export class AboutPage {
 
-  rssChannel: IRssChannel;
-
-  constructor(private navController: NavController, private rss: RssService) {
-    this.rssChannel = new RssChannel();
-    this.rss.getChannel("BUC_ACH_BUN").subscribe(channel => {
-      this.rssChannel = channel;
-    },
-      error => {
-        console.error(error);
-      });
+  constructor(private navController: NavController) {
   }
 }
