1fd3fbd7fd8d3925fa6b9e194f4071698715fa1f
Daniel <danieland.petrut@gmail.com>
2017-01-08 21:08:26 +0100

    fix imports after merge

--- anaf.app/src/app/app.module.ts
+++ anaf.app/src/app/app.module.ts
@@ -1,26 +1,57 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage, AboutPage, ContactPage,AsistentaPage } from './pages/index';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {
+    HomePage,
+    ContactPage,
+    ContactSediiPage,
+    ContactSesizarePage,
+    AsistentaPage,
+    InstrumentePage,
+    InfoPage,
+    TablouPage,
+    InstrumenteVerificareCifPage,
+    InstrumenteCalculatorVenitPage,
+    InstrumenteCalculatorTaxaAutoPage,
+    InstrumenteCalculatorTvaPage
+} from './pages/index';
+/*import {AboutPage} from "./pages/about/about.component";*/
 
 @NgModule({
     declarations: [
         MyApp,
         HomePage,
-        AboutPage,
         ContactPage,
-        AsistentaPage
+        ContactSediiPage,
+        ContactSesizarePage,
+        AsistentaPage,
+        InstrumentePage,
+        InfoPage,
+        TablouPage,
+        InstrumenteVerificareCifPage,
+        InstrumenteCalculatorVenitPage,
+        InstrumenteCalculatorTaxaAutoPage,
+        InstrumenteCalculatorTvaPage
     ],
-    imports: [ 
+    imports: [
         IonicModule.forRoot(MyApp)
     ],
     bootstrap: [IonicApp],
-    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }],
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}],
     entryComponents: [
         HomePage,
-        AboutPage,
         ContactPage,
-        AsistentaPage
+        ContactSediiPage,
+        ContactSesizarePage,
+        AsistentaPage,
+        InstrumentePage,
+        InfoPage,
+        TablouPage,
+        InstrumenteVerificareCifPage,
+        InstrumenteCalculatorVenitPage,
+        InstrumenteCalculatorTaxaAutoPage,
+        InstrumenteCalculatorTvaPage
     ]
 })
-export class AppModule { } 
+export class AppModule {
+}
